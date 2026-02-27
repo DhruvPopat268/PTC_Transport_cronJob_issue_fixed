1ffa1e68e5b3cd71ad9a4ec0f9cb9ce0d6f61f0b
Dhruv Popat <dhruvpopat268@gmail.com>
2026-02-27 18:32:47 +0530

    updated code to fix the issue

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -1,13 +1,14 @@
 // db.js
 
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 const mysql = require("mysql2/promise");
 
 // Create MySQL Pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD.replace(/"/g, ""), // remove quotes automatically
+  password: process.env.DB_PASSWORD?.replace(/"/g, "") || process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
