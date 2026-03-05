b6dd3941ab4ca77b6efb25b7f11596b39a674ba9
Dhruv popat <dhruvpopat268@gmail.com>
2026-03-05 11:24:50 +0530

    updated code to force the db timezone to UTC

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -11,6 +11,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD?.replace(/"/g, "") || process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+  timezone: '+00:00', // Force UTC
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
